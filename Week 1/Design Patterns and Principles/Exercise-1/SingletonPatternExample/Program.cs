b299e031c1ec06cab45bae3229ba131e51d1ea4d
Dilshad Alam <alamdilshad0449@gmail.com>
2026-06-24 20:32:18 +0530

    Initial commit

---
+++ Week 1/Design Patterns and Principles/Exercise-1/SingletonPatternExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger log1 = Logger.GetInstance();
+        Logger log2 = Logger.GetInstance();
+
+        log1.Log("First message");
+        log2.Log("Second message");
+
+        // Check whether both references point to same object
+        if (log1 == log2)
+        {
+            Console.WriteLine("Only one Logger instance exists.");
+        }
+        else
+        {
+            Console.WriteLine("Multiple instances created.");
+        }
+    }
+}
---
+++ Week 1/Design Patterns and Principles/Exercise-1/SingletonPatternExample/logger.cs
@@ -0,0 +1,30 @@
+using System;
+
+public sealed class Logger
+{
+    // Private static instance
+    private static Logger instance;
+
+    // Private constructor
+    private Logger()
+    {
+        Console.WriteLine("Logger instance created.");
+    }
+
+    // Public static method to get instance
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    // Logging method
+    public void Log(string message)
+    {
+        Console.WriteLine("[LOG]: " + message);
+    }
+}
